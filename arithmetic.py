74953a02884edaa3335276dba8624972d5e09a37
Christina Serveas P <christinaserveas@gmail.com>
2022-10-14 12:57:15 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -1,16 +1,16 @@
 def summation(a,b):
   c = a+b
-  print (c)
+  print (f"the value of c: {c}")
   
   def subraction(d,e):
   f = d-e
-  print(f)
+  print(f"the value of f: {f})
   
  var_a = 'please enter value '
 var_b = 'please enter value'
 
-sum.summation(var_a,var_b)
-subraction(var_a,var_b)
+sum.summation(a,b)
+subraction(d,e)
   
 
   
